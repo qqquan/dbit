6797d6f761d9d512ddb1e0effc99b293b07b52cd
qH <quans.box@gmail.com>
2021-07-15 20:02:47 -0400

    remove empty spaces

--- src/parsebit.cpp
+++ src/parsebit.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 void parse_bit32(uint32_t val)
 {
-
   constexpr auto bit_length = 32u;
   
   bitset<bit_length>data {val};
@@ -28,15 +27,15 @@ void parse_bit32(uint32_t val)
   cout << "Spaced Binary: " << bit_str << endl;
 
 
-  //build upper 16bit
+  //build upper 16bit table
   vector<std::string> vt_header16_31;
 
   for (int i = 15+16; i>=0+16; i--)
   {
     vt_header16_31.push_back("bit " + std::to_string(i) );
   }
-  VariadicTable<int,int,int,int,int,int,int,int,int,int,int,int,int,int,int,int> vt_upper16(vt_header16_31);
 
+  VariadicTable<int,int,int,int,int,int,int,int,int,int,int,int,int,int,int,int> vt_upper16(vt_header16_31);
 
   vt_upper16.addRow(data[15+16], data[14+16],data[13+16],data[12+16],data[11+16],data[10+16],data[9+16],data[8+16],data[7+16],data[6+16],data[5+16],data[4+16],data[3+16],data[2+16],data[1+16],data[0+16]);
 
@@ -55,8 +54,8 @@ void parse_bit32(uint32_t val)
     }
     vt_header0_15.push_back("bit " + width_compen + std::to_string(i));
   }
-  VariadicTable<int,int,int,int,int,int,int,int,int,int,int,int,int,int,int,int> vt_lower16(vt_header0_15);
 
+  VariadicTable<int,int,int,int,int,int,int,int,int,int,int,int,int,int,int,int> vt_lower16(vt_header0_15);
 
   vt_lower16.addRow(data[15], data[14],data[13],data[12],data[11],data[10],data[9],data[8],data[7],data[6],data[5],data[4],data[3],data[2],data[1],data[0]);
 
